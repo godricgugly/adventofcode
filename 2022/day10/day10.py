63b553e0df792393fd2369fc8f850b6525eb55d6
gugly <liamwatford@protonmail.com>
2023-01-06 14:57:06 +0100

    Erased 'test'

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -21,12 +21,10 @@ result = sum([h[20]*20, h[60]*60, h[100]*100, h[140]*140, h[180]*180, h[220]*220
 print("Part 1:", result)
 
 ### Part 2
-test = []
 displayCTR = np.zeros((6, 40))
 for y in range(6):
     for x in range(41):
         sprite = [h[y*40+x]-1, h[y*40+x], h[y*40+x]+1]
-        test.append([x-1, sprite])
         if x-1 in sprite:
             displayCTR[y][x-1] = 1
 
